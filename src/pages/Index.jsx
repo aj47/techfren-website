a671d1c571ed8912c903016b15a70eda56abbb27
Arash Joobandi <yspdev@gmail.com>
2024-02-07 13:31:04 +1100

    changes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,62 +1,94 @@
-import { Box, Container, Heading, SimpleGrid, Text, Link, Image, VStack, HStack, IconButton, useColorMode, useColorModeValue, Switch } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  Link,
+  Image,
+  VStack,
+  HStack,
+  IconButton,
+  useColorMode,
+  useColorModeValue,
+  Switch,
+} from "@chakra-ui/react";
 import { FaGithub, FaExternalLinkAlt, FaMoon, FaSun } from "react-icons/fa";
 
 const projects = [
   {
-    name: "Project Alpha",
-    description: "A revolutionary project that changes the way we interact with technology.",
-    image: "https://images.unsplash.com/photo-1527335480088-278dbeec0ad5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxzb2Z0d2FyZSUyMGFwcCUyMGludGVyZmFjZXxlbnwwfHx8fDE3MDUyMTk1MDB8MA&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/project-alpha",
-    live: "#",
+    name: "Autogen Skill Repository",
+    description: "A repository of python skills that can be added to Autogen",
+    image:
+      "https://images.unsplash.com/photo-1527335480088-278dbeec0ad5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxzb2Z0d2FyZSUyMGFwcCUyMGludGVyZmFjZXxlbnwwfHx8fDE3MDUyMTk1MDB8MA&ixlib=rb-4.0.3&q=80&w=1080",
+    github: "https://github.com/aj47/autogen-studio-skills",
+    live: "",
   },
   {
-    name: "Beta System",
-    description: "A robust system offering scalable solutions for enterprise needs.",
-    image: "https://images.unsplash.com/photo-1556761175-5973dc0f32e7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxlbnRlcnByaXNlJTIwc29mdHdhcmUlMjBhcHB8ZW58MHx8fHwxNzA1MjE5NTAwfDA&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/beta-system",
-    live: "#",
+    name: "LM Studio Preset Repository",
+    description:
+      "A robust system offering scalable solutions for enterprise needs.",
+    image:
+      "https://images.unsplash.com/photo-1556761175-5973dc0f32e7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxlbnRlcnByaXNlJTIwc29mdHdhcmUlMjBhcHB8ZW58MHx8fHwxNzA1MjE5NTAwfDA&ixlib=rb-4.0.3&q=80&w=1080",
+    github: "https://github.com/aj47/lm-studio-presets",
+    live: "",
   },
   {
-    name: "Gamma Tracker",
-    description: "An intuitive tracker that simplifies project management and team collaboration.",
-    image: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxwcm9qZWN0JTIwbWFuYWdlbWVudCUyMGFwcHxlbnwwfHx8fDE3MDUyMTk1MDF8MA&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/gamma-tracker",
-    live: "#",
+    name: "BeriGame MMO Alpha",
+    description: "A proof of concept cross platform MMO Game",
+    image:
+      "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxwcm9qZWN0JTIwbWFuYWdlbWVudCUyMGFwcHxlbnwwfHx8fDE3MDUyMTk1MDF8MA&ixlib=rb-4.0.3&q=80&w=1080",
+    github: "",
+    live: "www.alpha.berigame.com",
   },
   {
-    name: "Delta Exchange",
-    description: "A secure platform for digital asset exchange and cryptocurrency trading.",
-    image: "https://images.unsplash.com/photo-1627253781598-63b98c51da42?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxjcnlwdG9jdXJyZW5jeSUyMGFwcHxlbnwwfHx8fDE3MDUyMTk1MDF8MA&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/delta-exchange",
-    live: "#",
-  },
-  {
-    name: "Epsilon Notes",
-    description: "A note-taking app that organizes your thoughts and ideas efficiently.",
-    image: "https://images.unsplash.com/photo-1636297461709-0812290a9dcc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxub3RlJTIwdGFraW5nJTIwYXBwfGVufDB8fHx8MTcwNTIxOTUwMXww&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/epsilon-notes",
-    live: "#",
-  },
-  {
-    name: "Zeta Stream",
-    description: "A streaming service that brings you closer to the content you love.",
-    image: "https://images.unsplash.com/photo-1523365280197-f1783db9fe62?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxzdHJlYW1pbmclMjBzZXJ2aWNlJTIwYXBwfGVufDB8fHx8MTcwNTIxOTUwMXww&ixlib=rb-4.0.3&q=80&w=1080",
-    github: "https://github.com/arashjoobandi/zeta-stream",
-    live: "#",
+    name: "TimestampGenius.com",
+    description: "Automatically Generate Timestamps for Youtube Videos with AI",
+    image:
+      "https://images.unsplash.com/photo-1627253781598-63b98c51da42?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDcxMzJ8MHwxfHNlYXJjaHwxfHxjcnlwdG9jdXJyZW5jeSUyMGFwcHxlbnwwfHx8fDE3MDUyMTk1MDF8MA&ixlib=rb-4.0.3&q=80&w=1080",
+    github: "",
+    live: "www.timestampgenius.com",
   },
 ];
 
 const ProjectCard = ({ project }) => {
   return (
-    <VStack p={4} boxShadow="md" borderWidth="1px" borderRadius="lg" align="start">
-      <Image src={project.image} borderRadius="md" boxSize="100%" objectFit="cover" />
+    <VStack
+      p={4}
+      boxShadow="md"
+      borderWidth="1px"
+      borderRadius="lg"
+      align="start"
+    >
+      <Image
+        src={project.image}
+        borderRadius="md"
+        boxSize="100%"
+        objectFit="cover"
+      />
       <Heading size="md" mt={2}>
         {project.name}
       </Heading>
       <Text fontSize="sm">{project.description}</Text>
       <HStack>
-        <IconButton as={Link} href={project.github} icon={<FaGithub />} aria-label="GitHub" isExternal />
-        <IconButton as={Link} href={project.live} icon={<FaExternalLinkAlt />} aria-label="Live Demo" isExternal />
+        {project.github && (
+          <IconButton
+            as={Link}
+            href={project.github}
+            icon={<FaGithub />}
+            aria-label="GitHub"
+            isExternal
+          />
+        )}
+        {project.live && (
+        <IconButton
+          as={Link}
+          href={project.live}
+          icon={<FaExternalLinkAlt />}
+          aria-label="Live Demo"
+          isExternal
+        />
+        )}
       </HStack>
     </VStack>
   );
@@ -64,7 +96,18 @@ const ProjectCard = ({ project }) => {
 
 const ColorModeSwitcher = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  return <Switch position="fixed" top="1rem" right="1rem" color="green" isChecked={colorMode === "dark"} onChange={toggleColorMode} icon={colorMode === "light" ? <FaMoon /> : <FaSun />} size="lg" />;
+  return (
+    <Switch
+      position="fixed"
+      top="1rem"
+      right="1rem"
+      color="green"
+      isChecked={colorMode === "dark"}
+      onChange={toggleColorMode}
+      icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+      size="lg"
+    />
+  );
 };
 
 const Index = () => {
